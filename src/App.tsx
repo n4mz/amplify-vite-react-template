e55cf30eb0a2f7bae56153128382bcbe92754ec3
n4mz <namz1210@gmail.com>
2025-05-03 22:44:40 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import { useEffect, useState } from 'react';
 import { generateClient } from "aws-amplify/data";
+import type { Schema } from "../amplify/data/resource";
 
+// Initialize the client
 const client = generateClient<Schema>();
 
 function App() {
@@ -11,27 +11,41 @@ function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+
+    // Cleanup the subscription on component unmount
+    return () => {
+      subscription.unsubscribe();
+    };
   }, []);
 
-    function createTodo() {
+  function createTodo() {
     const content = window.prompt("Todo content");
     if (content) {
-      client.models.Todo.create({ content });
+      client.models.Todo.create({ content }).then(() => {
+        // Optionally refetch or add the new todo to the state
+        setTodos((prevTodos) => [
+          ...prevTodos,
+          { id: String(prevTodos.length + 1), content }, // Dummy id for illustration
+        ]);
+      });
     }
   }
 
   function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
+    client.models.Todo.delete({ id }).then(() => {
+      // Update the state to remove the deleted todo
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    });
   }
 
   return (
     <main>
-            <button onClick={signOut}>Sign out</button>
+      <button onClick={signOut}>Sign out</button>
       <h1>My todos</h1>
-             <button onClick={createTodo}>+ new</button>
+      <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
@@ -49,4 +63,5 @@ function App() {
     </main>
   );
 }
+
 export default App;
